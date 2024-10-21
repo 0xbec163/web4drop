8622e6dd4245dca6018bc366940e917bd32a6bb8
Kevin Harris <kharris20@cvtc.edu>
2024-10-21 09:25:10 -0500

    Add php usage examples

---
+++ 2-generated.php
@@ -0,0 +1,21 @@
+<?php
+  echo "<!DOCTYPE html>\n";
+  echo "<html>\n";
+  echo "  <head>\n";
+  echo "    <title>HTML/CSS/JS from PHP</title>\n";
+  echo "    <meta charset=\"utf-8\">\n";
+  echo "    <style>\n";
+  echo "      body {\n";
+  echo "        background-color: #eee;\n";
+  echo "        color: #003;\n";
+  echo "      }\n";
+  echo "    </style>\n";
+  echo "  </head>\n";
+  echo "  <body>\n";
+  echo "    <h1>Hello from PHP!</h1>\n";
+  echo "  <script>\n";
+  echo "    console.log(\"Hello from JavaScript via PHP!\");\n";
+  echo "  </script>\n";
+  echo "  </body>\n";
+  echo "</html>\n";
+?>
---
+++ 2-interleaved.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>HTML/CSS/JS from <?php echo 'PHP'; ?></title>
+    <meta charset="utf-8">
+    <style>
+      body {
+        background-color: #<?php echo 'eee'; ?>;
+        color: #003;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Hello from PHP!</h1>
+  <script>
+    console.log("<?php echo 'Hello from JavaScript via PHP!'; ?>");
+  </script>
+  </body>
+</html>
